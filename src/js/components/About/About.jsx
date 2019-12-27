3eec262ab2b08aee43e9d3165e1d0612e64edfe5
Hunter Jennings <hunterjennings@Hunters-MacBook-Pro-2.local>
2019-12-27 08:47:22 -0500

    uninstalling some unneeded packagages, adding some general info and utils

--- src/js/components/About/About.jsx
+++ src/js/components/About/About.jsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import sharedStyles from '../../../styles/shared/shared.scss';
 import Navigation from '../Navigation';
+import util from '../../utils/utilExample';
 
 function About() {
+  useEffect(() => {
+    util();
+  }, []);
+
   return (
     <main className={sharedStyles.mainContainer}>
       <h1>About Page</h1>
